a74cf4333056cfa6704e5f1a41a253903dcdce61
kgnblg <kgnblg@gmail.com>
2018-09-28 09:33:37 +0300

    qwe

--- p2testfortime.sh
+++ p2testfortime.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/pi/Desktop/phototaker.sh
 
-deger=1
-while [ $deger -lt 2 ]
-do
-
-sleep 5
-
-sudo fswebcam -p YUYV -d /dev/video0 -r 640x480 /home/pi/Desktop/5minupdates/imagesa.jpg
+sudo python /home/pi/Desktop/mot_det.py
+sleep 2
 sudo cp /home/pi/Desktop/5minupdates/imagesa.jpg /home/pi/Desktop/faceanalyze/face.jpg
 sleep 2
 sudo curl --form photo=@/home/pi/Desktop/5minupdates/imagesa.jpg "http://kaganbalga.com/bitirme/gateway.php?token=a&service=media&params=upload/1/0"
@@ -28,5 +23,3 @@ sudo rm /home/pi/Desktop/originalfaces/face.jpg
 fi
 
 sudo rm /home/pi/Desktop/faceanalyze/face.jpg
-
-done
